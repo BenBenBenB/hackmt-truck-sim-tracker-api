21b228c39e0a90b8b1d11751ee26d11779293877
Ben Burton <benjaminburton@gmail.com>
2024-01-26 22:37:20 -0500

    Default app from VS

---
+++ TruckSimTracker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TruckSimTracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

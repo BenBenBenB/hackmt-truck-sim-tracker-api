2877cc8c25e7970805155dbd0d9001d90cc8568e
Luka Howard <ljh4j@mtmail.mtsu.edu>
2024-01-27 13:54:18 -0600

    Added Job and Achive class

---
+++ TruckSimTracker.Data/Model/Achive.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace TruckSimTracker.Data.Models;
+public class Achivements : ITruckSimTrackerDataModel
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public DateTime Updated { get; set; } = DateTime.UtcNow;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty; 
+    [ForeignKey(typeof(Cargo))]
+    public int cargoId { get; set; }
+    [ForeignKey(typeof(stte))]
+    public int stateId { get; set; }
+    [ForeignKey(typeof(city))]
+    public int cityId { get; set; }
+
+}
---
+++ TruckSimTracker.Data/Model/Job.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace TruckSimTracker.Data.Models;
+public class job : ITruckSimTrackerDataModel
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public DateTime Updated { get; set; } = DateTime.UtcNow;
+    public string Company { get; set; } = string.Empty;
+
+    [ForeignKey(typeof(Cargo))]
+    public int cargoid { get; set; }
+    public int value { get; set; }
+
+    [ForeignKey(typeof(city))]
+    public int stratcityId { get; set; }
+    [ForeignKey(typeof(city))]
+    public int endcityId { get; set; }
+    public int pay { get; set; }
+
+    public bool nodamage_on_job { get; set; }
+
+
+}
